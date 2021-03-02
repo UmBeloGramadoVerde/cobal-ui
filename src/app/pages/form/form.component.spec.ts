f233b2b55a511985465ea1ece1fdbda8ae1eaf26
UmBeloGramadoVerde <kaynarodrigues@live.com>
2021-03-02 22:18:34 +0000

    Fixed tests

--- src/app/pages/form/form.component.spec.ts
+++ src/app/pages/form/form.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { FormComponent } from './form.component';
+import { FormsModule } from '@angular/forms';
 
 describe('FormComponent', () => {
   let component: FormComponent;
@@ -8,9 +9,9 @@ describe('FormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormComponent ]
-    })
-    .compileComponents();
+      declarations: [FormComponent],
+      imports: [RouterTestingModule, FormsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/pages/team/shared/team-card/team-card.component.spec.ts
+++ src/app/pages/team/shared/team-card/team-card.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { TeamCardComponent } from './team-card.component';
+import { FormDataModel } from 'src/app/pages/form/form.constants';
 
 describe('TeamCardComponent', () => {
   let component: TeamCardComponent;
@@ -8,14 +9,25 @@ describe('TeamCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TeamCardComponent ]
-    })
-    .compileComponents();
+      declarations: [TeamCardComponent],
+      imports: [RouterTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TeamCardComponent);
     component = fixture.componentInstance;
+    component.teamMember = {
+      emp_id: '',
+      name: '',
+      birthdate: '',
+      address: '',
+      status: '',
+      position: '',
+      created: '',
+      updated: '',
+      img: '',
+    };
     fixture.detectChanges();
   });
 
